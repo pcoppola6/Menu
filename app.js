deb2eaf00a0e0a1dad46a5f83a9b28b99c430978
Pasquale <pasquale.coppola6@gmail.com>
2022-10-26 10:49:47 -0400

    menu updates filter

--- app.js
+++ app.js
@@ -74,10 +74,16 @@ const menu = [
   ];
 
   const sectionCenter = document.querySelector('.section-center');
-
+  const container = document.querySelector('.btn-container');
+ 
+  
+  //load items
   window.addEventListener('DOMContentLoaded', function() {
-    displayMenuItems(menu);
+      displayMenuItems(menu);
+      displayMenuButtons();
   });
+
+
 
   function displayMenuItems(menuItems) {
     let displayMenu = menuItems.map(function (item) {
@@ -96,4 +102,40 @@ const menu = [
     displayMenu = displayMenu.join("");
     
     sectionCenter.innerHTML = displayMenu;
+  }
+
+  function displayMenuButtons() {
+    const categories = menu.reduce(function(values, item) {
+      if(!values.includes(item.category)){
+        values.push(item.category);
+      }
+      return values;
+    },
+      ['all']
+    );
+    const categoryBtns = categories.map(function(category) {
+      return  `<button class="filter-btn" type="button" 
+      data-id=${category}>
+      ${category}
+      </button>`;
+    }).join('');
+    container.innerHTML = categoryBtns;
+    const filterBtns = document.querySelectorAll('.filter-btn');
+
+      //filter items
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem) {
+        if(menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      if(category === 'all') {
+        displayMenuItems(menu);
+      } else {
+        displayMenuItems(menuCategory);
+      }
+    });
+  });
   }
